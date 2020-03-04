985e635d4b22617f995a4e82dce745f9469b4458
Alexander Ignatov <ani1802@abv.bg>
2020-03-04 23:53:14 +0200

    Don't allow multiple accounts with same email pt.2

--- api/endpoints/update_user.php
+++ api/endpoints/update_user.php
@@ -15,11 +15,18 @@ if (!$token_data) {
 $post_data = json_decode(file_get_contents("php://input"));
 
 $user = new User($db);
+$user->email = $post_data->email ?? "";
+
+if ($user->emailExists() && $user->id != $token_data->id) {
+    $response = new Response(401, "E-mail is already taken!");
+    $response->send();
+}
+
 $user->firstname = $post_data->firstname ?? "";
 $user->lastname = $post_data->lastname ?? "";
-$user->email = $post_data->email ?? "";
 $user->password = $post_data->password ?? null;
 $user->id = $token_data->id;
+
 
 if (!$user->update()) {
     $response = new Response(401, "Unable to update user.");
