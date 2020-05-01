e315612148ff11a550ed7a0b1b66a389ed3b03a3
Alexander Ignatov <ani1802@abv.bg>
2020-05-01 10:45:34 +0300

    Sort search results by levenshtein distance

--- endpoints/GET/search.php
+++ endpoints/GET/search.php
@@ -17,6 +17,27 @@ if ($dishes === false) {
     $dishes = [];
 }
 
+// Sort results
+
+function comparator($a, $b) {
+    global $term;
+
+    if ($a == $b) {
+        return 0;
+    }
+
+    $lev_a = levenshtein($a->name, $term);
+    $lev_b = levenshtein($b->name, $term);
+
+    if ($lev_a == $lev_b) {
+        return 0;
+    }
+
+    return ($lev_a < $lev_b) ? -1 : 1;
+}
+
+usort($dishes, "comparator");
+
 $response = new DataResponse("Search results ready.", $dishes);
 $response->send();
 
